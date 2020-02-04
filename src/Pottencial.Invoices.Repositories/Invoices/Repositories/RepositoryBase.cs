28048ee8e0e95c1260f77282aa6ee112a018e39a
Marcus Coelho <marcus.coelho@squadra.com.br>
2020-02-04 19:31:05 -0300

    Refatoração dos métodos virtuais e criação dos métodos protected virtual (Open-closed).

--- src/Pottencial.Invoices.Repositories/Invoices/Repositories/RepositoryBase.cs
+++ src/Pottencial.Invoices.Repositories/Invoices/Repositories/RepositoryBase.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 
 namespace Pottencial.Invoices.Repositories.Invoices.Repositories
@@ -13,16 +14,40 @@ namespace Pottencial.Invoices.Repositories.Invoices.Repositories
             Context = context;
         }
 
-        public virtual async Task Insert(T entity)
+        public async Task Insert(T entity)
         {
+            BeforeInsert(entity);
+            
             Context.Add(entity);
             await Context.SaveChangesAsync();
+
+            AfterInsert(entity);
         }
 
-        public virtual async Task Update(T entity)
+        protected virtual void AfterInsert(T entity)
         {
+        }
+
+        protected virtual void BeforeInsert(T entity)
+        {
+        }
+
+        public async Task Update(T entity)
+        {
+            BeforeUpdate(entity);
+
             Context.Entry(entity).State = EntityState.Modified;
             await Context.SaveChangesAsync();
+
+            AfterUpdate(entity);
+        }
+
+        protected virtual void BeforeUpdate(T entity)
+        {
+        }
+
+        protected virtual void AfterUpdate(T entity)
+        {
         }
 
         public virtual async Task Delete(T entity)
